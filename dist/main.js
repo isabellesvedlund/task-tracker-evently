6a71c8252149d8036c01f634a1fccc586e8f49e6
isabellesvedlund <isabellemariensvedlund@gmail.com>
2026-06-18 15:46:08 +0200

    header, array and for loop for printing tasks and how many

--- dist/main.js
+++ dist/main.js
@@ -9,5 +9,17 @@ function taskTracker() {
 `);
 }
 taskTracker();
+const tasks = [
+    "Lära mig TypeScript",
+    "Träna",
+    "Handla",
+    "Tvätta",
+    "Plugga",
+    "Borsta Tänderna",
+];
+for (let i = 0; i < tasks.length; i++) {
+    console.log(`${i + 1}. ${tasks[i]}`);
+}
+console.log(`Antal uppgifter: ${tasks.length}`);
 export {};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,19 @@ function taskTracker(): void {
 ==================================
 `);
 }
-
 taskTracker();
+
+const tasks: string[] = [
+  "Lära mig TypeScript",
+  "Träna",
+  "Handla",
+  "Tvätta",
+  "Plugga",
+  "Borsta Tänderna",
+];
+
+for (let i = 0; i < tasks.length; i++) {
+  console.log(`${i + 1}. ${tasks[i]}`);
+}
+
+console.log(`Antal uppgifter: ${tasks.length}`);
